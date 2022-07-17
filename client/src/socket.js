940d8f32a2234389d6f0cbc0d725be1c94dc6f83
CyberAvian <cyberavian025@gmail.com>
2022-07-17 03:01:29 -0400

    Fixing issues with sockets on heroku

--- client/src/socket.js
+++ client/src/socket.js
@@ -3,9 +3,10 @@ import io from 'socket.io-client';
 
 var port = process.env.REACT_APP_PORT || 9000;
 var serverAddress = 'http://localhost:' +  port.toString();
-console.log(`Port: ${port} Address: ${serverAddress}`);
+console.log(`Test Port: ${process.env.REACT_APP_PORT} Port: ${port} Address: ${serverAddress}`);
 
-const socket = io(serverAddress, { transports: ['websocket'] });
+// const socket = io(serverAddress, { transports: ['websocket'] });
+const socket = io("/");
 const SocketContext = React.createContext();
 
 export { socket, SocketContext };
