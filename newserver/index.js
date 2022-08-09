bd0caf760abaac9350956ec416e476c994fb9305
CyberAvian <cyberavian025@gmail.com>
2022-08-08 20:22:34 -0400

    started socket logic for rooms

--- newserver/index.js
+++ newserver/index.js
@@ -1,12 +1,15 @@
 const httpServer = require('http').createServer();
 const io = require('socket.io')(httpServer, {
+  // Need to use cors so the frontend can communicate with the backend
   cors: {
     origin: "http://localhost:8080",
   },
 });
 
+// Middleware to authenticate username. Password not necessary
 io.use((socket, next) => {
   const username = socket.handshake.auth.username;
+  // This check also happens on the frontend. This is an extra layer of security to prevent empty usernames
   if (!username) {
     console.log('invalid username');
     return next(new Error("invalid username"));
@@ -17,6 +20,7 @@ io.use((socket, next) => {
 
 io.on('connection', (socket) => {
   const users = [];
+  // Gather all users connected to the current socket server
   for (let [id, socket] of io.of('/').sockets) {
     users.push({
       userID: id,
@@ -24,6 +28,29 @@ io.on('connection', (socket) => {
     });
   }
   socket.emit("users", users);
+
+  // Broadcast to everyone but the current user
+  socket.broadcast.emit("user connected", {
+    userID: socket.id,
+    username: socket.username,
+  });
+
+  // Handle private messages between two users
+  socket.on("private message", ({ message, recepient }) => {
+    socket.to(recepient).emit("private message", {
+      message,
+      from: socket.id,
+    });
+  });
+
+  // For now, disconnected users are removed from the users list
+  socket.on("disconnect", () => {
+    socket.broadcast.emit("user disconnected", socket.id);
+    users.pop({
+      userID: socket.id,
+      username: socket.username,
+    });
+  });
 });
 
 const PORT = process.env.PORT || '9000';
